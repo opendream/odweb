30fcf3290e57d67cb6a472c5299e5445f49816ab
Keng Susumpow <patipat@me.com>
2026-06-19 18:30:17 +0700

    Revert "Add Cloudflare Workers configuration"
    This reverts commit b437bbcd88c4b1f9cddef24b2452f5e095b07b99.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: 'https://opendream.co.th',
-
   i18n: {
     defaultLocale: 'th',
     locales: ['th', 'en'],
     routing: { prefixDefaultLocale: false },
   },
-
   integrations: [sitemap(), mdx()],
-  adapter: cloudflare()
 });
